f752277ff57ce142d850fd72c05f0deae108cbd6
«Denis <6545158@gmail.com>
2023-01-30 23:45:23 +0300

    C# seminar 9 home task done

--- С#seminar9/Program.cs
+++ С#seminar9/Program.cs
@@ -71,11 +71,12 @@ Console.WriteLine($"Here is the sum of all natural numbers  between M and N both
 //m = 2, n = 3 -> A(m,n) = 9
 //m = 3, n = 2 -> A(m,n) = 29
 
-int AkkermanFunction(int m, int n){
+int AkkermanFunction(int m, int n)
+{
 
-  if(m>0 && n > 0) return AkkermanFunction(m-1,AkkermanFunction(m,n-1));
-  else if (m !=0 && n ==0) return AkkermanFunction(m-1,1);
-  else return n+1;
+    if (m > 0 && n > 0) return AkkermanFunction(m - 1, AkkermanFunction(m, n - 1));
+    else if (m != 0 && n == 0) return AkkermanFunction(m - 1, 1);
+    else return n + 1;
 }
 
 int m = -1;
@@ -98,4 +99,4 @@ do
 }
 while (n <= 0);
 
-Console.WriteLine("The Akkerman's function result for M,N is: "+ AkkermanFunction(m,n));
+Console.WriteLine("The Akkerman's function result for M,N is: " + AkkermanFunction(m, n));
